cf50b4f9442cd90cd412bedded7b6a12958b4d45
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2017-09-05 19:44:52 -0400

    Allow to customize max SMS per minute limit [1/2] Aosp has a fixed 30sms per minute value, stored in the Settings.java:
           public static final String SMS_OUTGOING_CHECK_MAX_COUNT =
                   "sms_outgoing_check_max_count";
    
    and src/java/com/android/internal/telephony/SmsUsageMonitor.java in the frameworks/opt/telephony:
        /** Default number of SMS sent in checking period without user permission. */
        private static final int DEFAULT_SMS_MAX_COUNT = 30;
    
    The 2/2 part was already made by CM guys:
    ezio84/abc_frameworks_opt_telephony@b3be4cb

--- src/com/aquarios/coralreef/tabs/SystemMiscTab.java
+++ src/com/aquarios/coralreef/tabs/SystemMiscTab.java
@@ -21,6 +21,7 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceScreen;
 
@@ -28,24 +29,63 @@ import com.android.internal.logging.nano.MetricsProto;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 public class SystemMiscTab extends SettingsPreferenceFragment
         implements OnPreferenceChangeListener {
+
+    private static final String APPS_SECURITY = "apps_security";
+    private static final String SMS_OUTGOING_CHECK_MAX_COUNT = "sms_outgoing_check_max_count";
+
+    private ListPreference mSmsCount;
+    private int mSmsCountValue;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.system_misc_tab);
         final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        PreferenceCategory appsSecCategory = (PreferenceCategory) findPreference(APPS_SECURITY);
+
+        mSmsCount = (ListPreference) findPreference(SMS_OUTGOING_CHECK_MAX_COUNT);
+        mSmsCountValue = Settings.Global.getInt(resolver,
+                Settings.Global.SMS_OUTGOING_CHECK_MAX_COUNT, 30);
+        mSmsCount.setValue(Integer.toString(mSmsCountValue));
+        mSmsCount.setSummary(mSmsCount.getEntry());
+        mSmsCount.setOnPreferenceChangeListener(this);
+        if (!Utils.isVoiceCapable(getActivity())) {
+            appsSecCategory.removePreference(mSmsCount);
+            prefScreen.removePreference(appsSecCategory);
+        }
     }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        // If we didn't handle it, let preferences handle it.
+        return super.onPreferenceTreeClick(preference);
+      }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.AQUA;
     }
 
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        final String key = preference.getKey();
-        return true;
-    }
-}
+     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        if (preference == mSmsCount) {
+            mSmsCountValue = Integer.valueOf((String) newValue);
+            int index = mSmsCount.findIndexOfValue((String) newValue);
+            mSmsCount.setSummary(
+                    mSmsCount.getEntries()[index]);
+            Settings.Global.putInt(resolver,
+                    Settings.Global.SMS_OUTGOING_CHECK_MAX_COUNT, mSmsCountValue);
+            return true;
+        }
+          return false;
+      }
+  }
